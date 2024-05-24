a6ba3ef8dd47e755da189e5eafe42f00b2c10480
Manuel Bucher <dev@manuelbucher.com>
2024-05-24 15:22:54 +0200

    Add optional parameters to only pull a selection of files

--- pull.bash
+++ pull.bash
@@ -4,5 +4,8 @@ API_KEY=$(cat ~/.config/sql-mozilla-api-key)
 
 while read -r OUT_FILE QUERY_ID
 do
-	curl "https://sql.telemetry.mozilla.org/api/queries/$QUERY_ID?api_key=$API_KEY" | jq .query --raw-output > $OUT_FILE
+	# https://superuser.com/a/186304
+	if [ "$#" -eq 0 ] || [[ "$*" == *"$QUERY_ID"* ]]; then
+		curl "https://sql.telemetry.mozilla.org/api/queries/$QUERY_ID?api_key=$API_KEY" | jq .query --raw-output > $OUT_FILE
+	fi
 done < query_list.tsv
--- push.bash
+++ push.bash
@@ -4,6 +4,9 @@ API_KEY=$(cat ~/.config/sql-mozilla-api-key)
 
 while read -r IN_FILE QUERY_ID
 do
-	echo $IN_FILE
-	jq -n --compact-output --monochrome-output --arg multiline "$(cat "$IN_FILE")" '{"query": $multiline}' | curl -X POST --data-binary @- "https://sql.telemetry.mozilla.org/api/queries/$QUERY_ID?api_key=$API_KEY"
+	# https://superuser.com/a/186304
+	if [ "$#" -eq 0 ] || [[ "$*" == *"$QUERY_ID"* ]]; then
+		echo $IN_FILE
+		jq -n --compact-output --monochrome-output --arg multiline "$(cat "$IN_FILE")" '{"query": $multiline}' | curl -X POST --data-binary @- "https://sql.telemetry.mozilla.org/api/queries/$QUERY_ID?api_key=$API_KEY"
+	fi
 done < query_list.tsv

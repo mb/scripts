f97cfcd73c5fc74af4893526fb770961940b16ee
Manuel Bucher <dev@manuelbucher.com>
2025-09-02 20:22:46 +0200

    Fix pre formatting

--- sah/src/main.rs
+++ sah/src/main.rs
@@ -452,7 +452,7 @@ impl Request {
         };
         let activate_header = if let Some(activate) = self.query.activate {
             format!(
-                r#"<pre>"Activate-Storage-Access: {}"</pre>"#,
+                r#"<pre style="display:inline">"Activate-Storage-Access: {}"</pre>"#,
                 activate.header_value()
             )
         } else {
